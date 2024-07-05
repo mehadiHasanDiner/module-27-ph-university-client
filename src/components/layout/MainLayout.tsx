41dc1fdd58e54f87c303e1f45a4ba658baad3eac
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2024-07-06 02:57:55 +0600

    module 28 running

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,6 +1,8 @@
 import { Layout, Menu, MenuProps } from "antd";
-import { NavLink, Outlet } from "react-router-dom";
-import { adminSidebarItems } from "../../routes/admin.routes";
+import { Outlet } from "react-router-dom";
+import { adminPaths } from "../../routes/admin.routes";
+import { sidebarItemGenerator } from "../../utils/sidebarItemsGenerator";
+import Sidebar from "./Sidebar";
 
 const { Content, Footer, Header, Sider } = Layout;
 
@@ -17,7 +19,7 @@ const { Content, Footer, Header, Sider } = Layout;
 //     key: "User Management",
 //     label: "User Management",
 //     children: [
- 
+
 //       {
 //         key: "Create Admin",
 //         label: <NavLink to="/admin/create-admin">Create Admin</NavLink>,
@@ -37,36 +39,7 @@ const { Content, Footer, Header, Sider } = Layout;
 const MainLayout = () => {
   return (
     <Layout style={{ height: "100vh" }}>
-      <Sider
-        breakpoint="lg"
-        collapsedWidth="0"
-        onBreakpoint={(broken) => {
-          console.log(broken);
-        }}
-        onCollapse={(collapsed, type) => {
-          console.log(collapsed, type);
-        }}
-      >
-        <div
-          style={{
-            color: "white",
-            textAlign: "center",
-            height: "4rem",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <h1>PH University</h1>
-        </div>
-
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={["4"]}
-          items={adminSidebarItems}
-        />
-      </Sider>
+      <Sidebar />
       <Layout>
         <Header style={{ padding: 0 }} />
         <Content style={{ margin: "24px 16px 0" }}>
@@ -80,9 +53,7 @@ const MainLayout = () => {
             <Outlet />
           </div>
         </Content>
-        <Footer style={{ textAlign: "center" }}>
-          Ant Design ©{new Date().getFullYear()} Created by Ant UED
-        </Footer>
+      
       </Layout>
     </Layout>
   );
